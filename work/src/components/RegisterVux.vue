f329ffc5d0b7ce619576129204ec6e16278989b9
nncrd <a877277@sina.com>
2017-07-17 23:28:56 +0800

    Signed-off-by: nncrd <a877277@sina.com>

---
+++ work/src/components/RegisterVux.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="register">
+      <img class="logo" src="../assets/vux_logo.png">
+      <h3></h3>
+      <divider>账号注册</divider>
+      <div>
+        <tab :line-width=2 active-color='#41b883' v-model="index">
+          <tab-item class="vux-center" :selected="activeType === item" v-for="(item, index) in registerType" @click="activeType = item" :key="index">{{item}}</tab-item>
+        </tab>
+        <swiper v-model="index" height="30rem" :show-dots="false">
+          <swiper-item v-for="(item, index) in registerType" :key="index">
+            <div class="tab-swiper vux-center">{{item}}内容</div>
+            <register-staff></register-staff>
+          </swiper-item>
+        </swiper>
+      </div>
+    </div>    
+  </div>
+</template>
+
+<script>
+const list = () => ['普通人员注册', '工作人员注册']
+import registerStaff from './registerStaff'
+import {Group, Cell, Tab, TabItem, Sticky, Divider, XButton, Swiper, SwiperItem} from 'vux'
+export default {
+  components: {  
+    registerStaff,  
+    Group,
+    Cell,
+    Tab,
+    TabItem,
+    Sticky,
+    Divider,
+    XButton,
+    Swiper,
+    SwiperItem    
+  },
+  data () {
+    return {
+      msg: 'Hello World!',
+      registerType: list(),
+      activeType: '普通人员注册',
+      index: 0,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.register {
+  text-align: center;
+}
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
---
+++ work/src/components/registerStaff.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div style="padding:15px;">
+      <input type="text" placeholder="地点">
+      <input type="text" placeholder="单位">
+      <input type="text" placeholder="手机号">
+      <input type="text" placeholder="姓名">	    
+      <x-button @click.native="showPlugin" type="primary">提交</x-button>
+    </div>
+     <div v-transfer-dom v-show="show1">
+      <confirm v-model="show"
+      :title = "title"
+      @on-cancel="onCancel"
+      @on-confirm="onConfirm"
+      @on-show="onShow"
+      @on-hide="onHide">
+        <p style="text-align:center;">内容</p>
+      </confirm>
+    </div>   
+  </div>
+</template>
+<style scoped>
+@import '../assets/css/RegisterStaff.css'
+</style>
+<script>
+import { Confirm, XButton ,TransferDomDirective as TransferDom} from 'vux'
+export default{
+  directives: {
+    TransferDom
+  },
+  components: {
+    XButton,
+    Confirm
+  },
+  data () {
+    return {
+      show: false,
+      show1:false,
+      title:"提示"
+    }
+  },
+  methods: {
+    onCancel () {
+      console.log('on cancel')
+    },
+    onConfirm (msg) {
+      console.log('on confirm')
+      if (msg) {
+        alert(msg)
+      }
+    },
+    onHide () {
+      console.log('on hide')
+    },
+    onShow () {
+      console.log('on show')
+    },
+    showPlugin () {
+      this.show1 = true;
+      this.show = true;
+    }
+  }
+}
+</script>
